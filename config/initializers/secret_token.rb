819430cc5ca613cd6190161d5efb310af76af116
Subhash Shah <subhash.shah@botreeconsulting.com>
2015-01-28 00:26:46 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LetsRead::Application.config.secret_key_base = '846c3ba787c7f58b29b87c9b1c9a03f47eeef9fee4dd5dc5f3e71f91e177aafc103e4fe40bdce8eaba551fd01207a1e4223ff7935c19a0002bc49f9c57423121'
